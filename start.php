c2cb14abc4aa2970c1200ff4d86001ea19222a96
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-08-05 01:57:53 +0200

    fix(access): use picture URL as link for non-public attachments

--- start.php
+++ start.php
@@ -31,11 +31,6 @@ function elgg_hybridauth_share_init() {
 
 	elgg_register_plugin_hook_handler('hybridauth:share', 'thewire', 'elgg_hybridauth_share_prepare_wall_post');
 	elgg_register_plugin_hook_handler('hybridauth:share', 'hjwall', 'elgg_hybridauth_share_prepare_wall_post');
-	
-	elgg_register_plugin_hook_handler('public_pages', 'walled_garden', 'elgg_hybridauth_share_public_pages');
-	elgg_register_plugin_hook_handler('view', 'resources/file/view', 'elgg_hybridauth_share_file_view');
-	elgg_register_plugin_hook_handler('head', 'page', 'elgg_hybridauth_share_metatags');
-	elgg_register_plugin_hook_handler('entity:icon:sizes', 'all', 'elgg_hybridauth_share_icon_sizes');
 
 	// Integrate with the wire form
 	elgg_extend_view('forms/thewire/add', 'hybridauth/share');
@@ -346,11 +341,6 @@ function elgg_hybridauth_share_prepare_wall_post($hook, $type, $return, $params)
 
 		if ($attachment->access_id == ACCESS_PUBLIC) {
 			$return['link'] = $attachment->getURL();
-		} else if (elgg_is_active_plugin('hypeDiscovery')) {
-			if (\hypeJunction\Discovery\is_discoverable_type($attachment)) {
-				$attachment->discoverable = true;
-				$return['link'] = \hypeJunction\Discovery\get_entity_permalink($attachment);
-			}
 		}
 
 		if (empty($return['link'])) {
@@ -361,132 +351,4 @@ function elgg_hybridauth_share_prepare_wall_post($hook, $type, $return, $params)
 	}
 
 	return $return;
-}
-
-function elgg_hybridauth_share_public_pages($hook, $type, $return, $params) {
-	
-	if (elgg_hybridauth_share_is_fb_user_agent()) {
-		$return[] = 'file/view/.*';
-	}
-    return $return;
-}
-
-/**
- * Is this a scrape by the Facebook preview generator?
- * 
- * @return boolean
- */
-function elgg_hybridauth_share_is_fb_user_agent() {
-	if (
-		strpos($_SERVER['HTTP_USER_AGENT'], "facebookexternalhit/") !== false ||
-		strpos($_SERVER['HTTP_USER_AGENT'], "Facebot") !== false
-	) {
-		return true;
-	}
-	
-	return false;
-}
-
-function elgg_hybridauth_share_metatags($hook, $type, $return, $params) {
-	if (!elgg_hybridauth_share_is_fb_user_agent()) {
-		return $return;
-	}
-
-	$wall_post = elgg_get_page_owner_entity();
-	if (!elgg_instanceof($wall_post, 'object', 'hjwall')) {
-		return $return;
-	}
-	
-	if ($attachments = $wall_post->getAttachments()) {
-		$attachment = array_shift($attachments);
-	}
-	
-	if (!$attachment) {
-		return $return;
-	}
-	
-	$icon = $attachment->getIcon('open_graph_image');
-	elgg_hybridauth_share_fb_icon_resize($icon);
-
-	$url = _elgg_services()->iconService->getIconURL($attachment, 'open_graph_image');
-	if ($url) {
-		$return['links']['canonical'] = ['rel' => 'canonical', 'href' => current_page_url()];
-		$return['metas']['og:image']['property'] = 'og:image';
-		$return['metas']['og:image']['content'] = $url;
-	}
-
-	return $return;
-}
-
-/**
- * intercept file view
- * 
- * @param type $hook
- * @param type $type
- * @param type $return
- * @param type $params
- */
-function elgg_hybridauth_share_file_view($hook, $type, $return, $params) {
-	if (!elgg_hybridauth_share_is_fb_user_agent()) {
-		return $return;
-	}
-	
-	return elgg_view_page('OG:Exemption', '');
-}
-
-//@TODO - this isn't really working - hypeDiscovery is all taking over this and who knows what's going on
-function elgg_hybridauth_share_icon_sizes($hook, $type, $return, $params) {
-	//@TODO - need to find a way to pad it to exact dimensions.  This just sets max w/h
-	$return['open_graph_image'] = [
-		'w' => 1200,
-		'h' => 630,
-		'square' => false,
-		'upscale' => true
-	];
-	
-	return $return;
-}
-
-function elgg_hybridauth_share_fb_icon_resize($icon) {
-	if (!$icon instanceof \ElggIcon) {
-		return;
-	}
-	
-	list($w, $h) = getimagesize($icon->getFilenameOnFilestore());
-	if ($w && $h) {
-		if ($w == 1200 && $h == 630) {
-			return;
-		}
-		
-		// we need to pad the image to that size
-		$output_w = 1200;
-		$output_h = 630;
-		
-		
-		$inner_w = $inner_h = min([max([$w, $h])]);
-		if ($h > $w) {
-			$scale = $inner_w/$h;
-		}
-		else {
-			$scale = $inner_h/$w;
-		}
-		
-		$new_w = $w * $scale;
-		$new_h = $h * $scale;
-		$offset_x = ($output_w - $w) / 2;
-		$offset_y = ($output_h - $h) / 2;
-
-		$src_image = imagecreatefromstring(file_get_contents($icon->getFilenameOnFilestore()));
-		
-		$image = imagecreatetruecolor($output_w, $output_h);
-		$bgcolor = imagecolorallocate($image, 255, 255, 255); // transparency
-		//$bgcolor = imagecolorallocate($image, 127, 127, 127);
-		imagefill($image, 0, 0, $bgcolor);
-		
-		// copy uploaded image onto the square background
-		imagecopyresampled($image, $src_image, $offset_x, $offset_y, 0, 0, $new_w, $new_h, $w, $h);
-		
-		imagejpeg($image, $icon->getFilenameOnFilestore());
-		imagedestroy($image);
-	}
 }
